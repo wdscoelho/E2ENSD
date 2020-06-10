fabdafbbff8548e3912547f9c6772b502a2c770a
Wesley da Silva Coelho <45847295+wdscoelho@users.noreply.github.com>
2020-06-10 11:41:43 +0200

    Update NSDP_instance_generator.jl

--- include/NSDP_instance_generator.jl
+++ include/NSDP_instance_generator.jl
@@ -1,6 +1,5 @@
 
 function physical_net_mandala(number_nodes::Int64, folder::String) 
-
     count_node_id = 1
     count_arc_id = 1
     number_arcs = number_nodes*6
@@ -8,7 +7,7 @@ function physical_net_mandala(number_nodes::Int64, folder::String)
             #creating access nodes
             for n in 1:trunc(Int,number_nodes)
                 write(io, "node $(count_node_id) node_id $(count_node_id) node_type access longitude $(rand(10:1000)/1.023) latitude $(rand(0:1000)/1.023) coverage_radius $(rand(100:1000)/1.03) ")
-                write(io, "ram_capacity $(rand(100:200)) cpu_capacity 400 storage_capacity  $(rand(100:200)) networking_capacity  $(rand(100:200)) ")
+                write(io, "ram_capacity $(rand(100:200)) cpu_capacity $(rand(400:400)) storage_capacity  $(rand(100:200)) networking_capacity  $(rand(100:200)) ")
                 write(io, "ram_cost $(rand(150:300)) cpu_cost $(rand(600:800)) storage_cost $(rand(1:2)) networking_cost $(rand(1:2)) ue_capacity $(rand(100:200)) availability 0.999 internal_delay 54 node_cost $(rand(100:200))\n\n")
                 count_node_id=count_node_id+1
             end
@@ -16,7 +15,7 @@ function physical_net_mandala(number_nodes::Int64, folder::String)
             #creating non-access nodes: agg
             for n in 1:trunc(Int,number_nodes/4)
                 write(io, "node $(count_node_id) node_id $(count_node_id) node_type non_access longitude $(rand(10:1000)/1.023) latitude $(rand(0:1000)/1.023) coverage_radius $(rand(100:1000)/1.03) ")
-                write(io, "ram_capacity $(rand(900:1100)) cpu_capacity 480 storage_capacity  $(rand(900:1100)) networking_capacity  $(rand(900:1100)) ")
+                write(io, "ram_capacity $(rand(900:1100)) cpu_capacity $(rand(480:480)) storage_capacity  $(rand(900:1100)) networking_capacity  $(rand(900:1100)) ")
                 write(io, "ram_cost $(rand(150:300)) cpu_cost $(rand(600:800)) storage_cost $(rand(1:2)) networking_cost $(rand(1:2)) ue_capacity $(rand(100:200)) availability 0.999 internal_delay 54 node_cost $(rand(100:200))\n\n")
                 count_node_id=count_node_id+1
             end
@@ -24,7 +23,7 @@ function physical_net_mandala(number_nodes::Int64, folder::String)
             #creating non-access nodes : core
             for n in 1:trunc(Int,number_nodes/4)
                 write(io, "node $(count_node_id) node_id $(count_node_id) node_type non_access longitude $(rand(10:1000)/1.023) latitude $(rand(0:1000)/1.023) coverage_radius $(rand(100:1000)/1.03) ")
-                write(io, "ram_capacity $(rand(900:1100)) cpu_capacity 560 storage_capacity  $(rand(900:1100)) networking_capacity  $(rand(900:1100)) ")
+                write(io, "ram_capacity $(rand(900:1100)) cpu_capacity $(rand(560:560)) storage_capacity  $(rand(900:1100)) networking_capacity  $(rand(900:1100)) ")
                 write(io, "ram_cost $(rand(150:300)) cpu_cost $(rand(600:800)) storage_cost $(rand(1:2)) networking_cost $(rand(1:2)) ue_capacity $(rand(100:200)) availability 0.999 internal_delay 54 node_cost $(rand(100:200))\n\n")
                 count_node_id=count_node_id+1   
             end
@@ -155,13 +154,11 @@ function physical_net_mandala(number_nodes::Int64, folder::String)
         write(io, "node_capacity_types 4 storage_capacity networking_capacity ram_capacity cpu_capacity\n\n")          
         write(io,ioo)
     end end
-                            
+                                                          
 end
 
 
-
 function slice_requests_generator(number_nodes::Int64, folder::String, number_commodities::Int64, number_slices::Int64)
-    
     for s in 1:number_slices
         open(joinpath(folder,"commodities_$(s).dat"), "w") do io
             write(io, "number_of_commodities $(trunc(Int,number_commodities))\n\n")
@@ -198,4 +195,5 @@ function slice_requests_generator(number_nodes::Int64, folder::String, number_co
             end
         end
 end
+
 
