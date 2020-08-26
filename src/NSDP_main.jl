6df9fbd9e7ea9cefe25f91e3aabd35df0c5f5541
Wesley da Silva Coelho <45847295+wdscoelho@users.noreply.github.com>
2020-08-26 11:38:30 +0200

    Update NSDP_main.jl

--- src/NSDP_main.jl
+++ src/NSDP_main.jl
@@ -1,9 +1,8 @@
-
 ##############################################################################
 # Created by: Wesley DA SILVA COELHO
 ##############################################################################
 #This file contains the main script for NSDP model execution
-#: https://hal.archives-ouvertes.fr/hal-02448028v3
+#: https://hal.archives-ouvertes.fr/hal-02448028
 
 
 ##############################################################################
@@ -12,7 +11,6 @@
 using NBInclude #Package to include personal files",
 using JuMP #Mathematical programming package : allows us to define the MILP",
 using LightGraphs #Package to construct simple graphs",
-using MetaGraphs #Package to construct complex graphs",
 
 
 # path to some folders
@@ -23,10 +21,10 @@ result_folder = "../Results/"
 
 
 #including files
-@nbinclude(joinpath(include_folder,"NSDP_instance_generator.jl"))# code creator for 5GNSD instances
-@nbinclude(joinpath(include_folder,"NSDP_structures.jl")) # our data structures 
-@nbinclude(joinpath(include_folder,"NSDP_solver.jl")) # impementation of our model 
-@nbinclude(joinpath(include_folder,"NSDP_instance_reader.jl"))# file responsible for parseing new NSDP instances
+@nbinclude(joinpath(include_folder,"NSDP_instance_generator.ipynb"))# code creator for 5GNSD instances
+@nbinclude(joinpath(include_folder,"NSDP_structures.ipynb")) # our data structures 
+@nbinclude(joinpath(include_folder,"NSDP_solver.ipynb")) # impementation of our model 
+@nbinclude(joinpath(include_folder,"NSDP_instance_reader.ipynb"))# file responsible for parseing new NSDP instances
 
 
 
@@ -39,38 +37,50 @@ number_slices=4
 physical_net_mandala(number_DUs,input_folder) 
 slice_requests_generator(number_DUs, input_folder, number_request_per_slice, number_slices)
 
+
 #sharing policies to be tested 
 sharing_policies = ["flatSharing","hard","sharedCP","sharedDP","partialCP","partialDP"]
 # split options: .... 7=flex
 split_settings = [1,2,3,4,5,6,7] 
 
-#creating files and their headres to store the final solutions
-#see the header (on write function) to know the information on each file 
+
+creating files and their headres to store the final solutions
+see the header (on write function) to know the information on each file 
 open(joinpath(result_folder,"load_on_nodes.csv"), "w") do finalNodeLoad
-    write(finalNodeLoad,"nodeID;type;policy;split;load\n")
+    write(finalNodeLoad,"topology;OF;test;nodeID;type;sharing_policy;split_setting;load\n")
 end
 
 open(joinpath(result_folder,"load_on_arcs.csv"), "w") do finalArcLoad
-    write(finalArcLoad,"arcID;type;policy;split;load\n") 
+    write(finalArcLoad,"topology;OF;test;arcID;type;sharing_policy;split_setting;load\n") 
 end
 
 open(joinpath(result_folder,"final_statistics.csv"), "w") do finalStatistics 
-    write(finalStatistics,"instance_name;policy;split;Objective_value;NFSintalled;disNFS;centNFS;nodes;activeNodes;acitiveNodeRatio;totalNodeCapacity;totalNodeLoad;avNodeLoad;mostLoadedNode;arc;activeArcs;acitiveArcRatio;totalArcCapacity;totalArcLoad;avArcLoad;mostLoadedArc\n")
+    write(finalStatistics,"topology;OF;test;instance_name;sharing_policy;split_setting;Objective_value;NFSintalled;disNFS;centNFS;nodes;activeNodes;acitiveNodeRatio;totalNodeCapacity;totalNodeLoad;avNodeLoad;mostLoadedNode;arc;activeArcs;acitiveArcRatio;totalArcCapacity;totalArcLoad;avArcLoad;mostLoadedArc;avrLat\n")
 end
 
-for policy in sharing_policies
 
-    #getting all proprities needed to represent the instance
-    my_instance = get_Instance(input_folder,policy)
+my_instance = 0
+OFs = ["minNFS"]
+variants = ["NDSP"]
+sharing_policies = ["hard","flatSharing","sharedCP","sharedDP","partialCP","partialDP"]
+sharing_policies = ["partialCP","partialDP"]
 
-    for split in split_settings  
-        #creating model
-        model = create_NSDP_model(my_instance, "option_$(split)")
-        #calling solver 
-        solve_NSDP_model(model,policy,split)
-        #getting and saving final solution and information
-        get_solution(model,my_instance,result_folder,split,policy)
-    end 
-
+splits = [1,2,3,4,5,6,7]
+for test in 1:10, of in OFs, var in variants, sh in sharing_policies, split in splits
+#getting all proprities needed to represent the instance
+    my_instance = 0
+    if var == "NDSP"
+        my_instance = get_Instance(input_folder,sh,test)
+    else
+        my_instance = get_Instance_pre(input_folder,"flatSharing")
+    end
+    #creating model
+    model = create_NSDP_variants_model(my_instance, var,of, "option_$split")
+    #calling solver 
+    solve_NSDP_model(model,"test$(test)_of$(of)_var$(var)_sh$(sh)_split$(split)")
+        
+    #getting and saving final solution and information
+    get_solution(model,my_instance,result_folder,sh,"split$split",test)
+    my_instance = 0
+    model = 0
 end
-
