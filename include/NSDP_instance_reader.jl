85445d1dcd0046b43fddb6328e5d0d8aabb9398b
Wesley da Silva Coelho <45847295+wdscoelho@users.noreply.github.com>
2020-08-26 11:41:36 +0200

    Update NSDP_instance_reader.jl

--- include/NSDP_instance_reader.jl
+++ include/NSDP_instance_reader.jl
@@ -15,13 +15,36 @@ using LightGraphs
 using MetaGraphs
 @nbinclude("NSDP_structures.ipynb")
 
-function get_Instance(input_folder::String,mp::String)
+function get_Instance_pre(input_folder::String,mp::String)
     
    #Here, we get all proprities needed to represent the instance
         set,anf,cnf = get_VNFs(joinpath(input_folder,"VNFs_instance_1.dat"))
         instance = Instance(set,
-                            get_physical_network(joinpath(input_folder,"my_phyNet.dat")),
-                            get_NS_requests(joinpath(input_folder,"instance_$(mp).dat")))
+                            get_physical_network(joinpath(input_folder,"my_phyNetITC.dat")),
+                            get_NS_requests(joinpath(input_folder,"instance_$(mp)_pre.dat")))
+
+        instance.VNF_connection = get_VNFs_To_Connect(instance)
+        instance.number_of_AN_based_NFs = anf
+        instance.number_of_CN_based_NFs = cnf
+        instance.maxLatencyBetweenFunctions = get_latency_between_functions(joinpath(input_folder,"latency_btw_functions_1.dat"))
+        number_commodities = 0
+
+        instance.number_of_NFs = 4
+        instance.slice_beta = get_slice_beta(joinpath(input_folder,"slice_beta.dat"))
+        instance.slice_rho = get_slice_rho(joinpath(input_folder,"slice_rho.dat"))
+        instance.maxLatencyBetweenDU_NFS1 = 0.25
+        
+        return instance
+    
+end
+
+function get_Instance(input_folder::String,mp::String,test::Int64)
+    
+   #Here, we get all proprities needed to represent the instance
+        set,anf,cnf = get_VNFs(joinpath(input_folder,"VNFs_instance_1.dat"))
+        instance = Instance(set,
+                            get_physical_network(joinpath(input_folder,"my_phyNetITC.dat")),
+                            get_NS_requests(joinpath(input_folder,"instance_$(mp).dat"),test))
 
         instance.VNF_connection = get_VNFs_To_Connect(instance)
         instance.number_of_AN_based_NFs = anf
@@ -31,15 +54,13 @@ function get_Instance(input_folder::String,mp::String)
         for s in 1:length(instance.setSlices)
             number_commodities+=length(instance.setSlices[s].set_commodities)
         end
-        instance.number_of_NFs = cnf*length(instance.setSlices) + anf*number_commodities#this is tha max number of NFs 
-        instance.slice_cont = get_slice_cont(joinpath(input_folder,"slice_cont_1.dat"))
-        instance.maxLatencyBetweenDU_NFS1 = 0.25
+        instance.number_of_NFs = 4
         
         return instance
     
 end
 
-function get_NS_requests(file::String) 
+function get_NS_requests(file::String,test::Int64) 
     #Auxiliar variables
     setSlices = Vector{SliceRequest}()
    # We open a text file found on the recieved path "file::String"
@@ -68,7 +89,7 @@ function get_NS_requests(file::String)
                 push!(setSlices, SliceRequest(parse(Int8,aux_string[4]),parse(Int16,aux_string[10]),
                                          parse(Float16,aux_string[12]),VNFs_to_install_instance_name,Meta.parse(aux_string[18]),
                                          Meta.parse(aux_string[20]), get_VNF_sharing(Meta.parse(aux_string[20])),
-                                        aux_set_VNFs_to_install,get_commodities(Meta.parse(aux_string[22])), parse(Float16,aux_string[24]),get_node_sharing(Meta.parse(aux_string[26]))))
+                                        aux_set_VNFs_to_install,get_commodities(Meta.parse(aux_string[22]),test), parse(Float16,aux_string[24]),get_node_sharing(Meta.parse(aux_string[26]))))
            
                 end# end of conditions   
                
@@ -79,7 +100,7 @@ function get_NS_requests(file::String)
 
 end # end of function get_NS_requests
 
-function get_slice_cont(file::String)
+function get_slice_beta(file::String)
     #auxiliar variables
     number_of_slices = 0
     aux_Matrix01 = Vector{ Vector{Int8} } 
@@ -105,6 +126,49 @@ function get_slice_cont(file::String)
             else
                 #we add its attributes
                 aux_vector = Array{Int8,1}(undef, number_of_slices)          
+
+                for (n, f) in enumerate(aux_string)
+                    aux_vector[n] = parse(Int8,f)
+                end  
+                 push!(aux_Matrix_return,aux_vector)       
+
+            end# end of conditions   
+
+        end#end for - we have read all lines from file   
+
+    end#closing file
+    
+    return aux_Matrix_return 
+
+end
+    
+    
+function get_slice_rho(file::String)
+    #auxiliar variables
+    number_of_dus = 0
+    aux_Matrix01 = Vector{ Vector{Int8} } 
+    aux_Matrix_return = []
+   
+    open(file) do file
+  
+        #we read line by line
+        for ln in eachline(file)
+
+            #Here, we storage each word / number in a position of aux_string vector
+            aux_string = split(ln)
+
+            #error treatment: if it is a line with nothing written or with ";", we return to the first isntruction of this loop
+            if length(aux_string) == 0
+                continue #going directly to the first line of the loop
+            end
+            
+            #getting number of VNFs 
+            if aux_string[1] == "number_of_dus" 
+                number_of_dus = parse(Int64,aux_string[2]) # Meta.parse tries to convert strings into a most suitable type
+
+            else
+                #we add its attributes
+                aux_vector = Array{Int8,1}(undef, number_of_dus)          
 
                 for (n, f) in enumerate(aux_string)
                     aux_vector[n] = parse(Int8,f)
@@ -233,7 +297,7 @@ function get_physical_network(file::String)
             #getting edges and their attributes. Here, we can add an edge and its attributes with the same function
             elseif aux_string[1]  == "arc"            
                 MetaGraphs.add_edge!(physical_network, Meta.parse(aux_string[6]), Meta.parse(aux_string[8]), Dict(:edge_id=>Meta.parse(aux_string[4]), :source =>Meta.parse(aux_string[6]), :target =>Meta.parse(aux_string[8]), :type =>aux_string[10], :delay =>Meta.parse(aux_string[12]), :max_bandwidth =>Meta.parse(aux_string[14]), :availability=>Meta.parse(aux_string[16]), :cost =>Meta.parse(aux_string[18])))
-           
+                 
             elseif aux_string[1]  == "node_capacity_types"
                 aux_vec = Vector{Symbol}()                
                 MetaGraphs.set_prop!(physical_network, :number_node_capacity_types, parse(Int8,aux_string[2]))              
@@ -447,12 +511,13 @@ function get_VNFs_To_Connect(instance::Instance)
 end#end of function
 
 
-function get_commodities(file::String) 
+function get_commodities(file1::String,test::Int64) 
     
     #auxiliar variables
     aux_vector = Vector{Dict}()
     number_of_commodities = Int16
-   
+    aux = split(file1,".dat")
+    file = "$(aux[1])_test$(test).dat"
    # We open a text file found on the recieved path "file::String"
     open(file) do file
         
